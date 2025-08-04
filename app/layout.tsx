b909a37b00977bed2c147e6bff3497060c76c938
docscodes <docscodes@gmail.com>
2025-08-04 11:50:08 -0400

    use system as defaultTheme

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
